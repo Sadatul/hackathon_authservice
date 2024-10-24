80decf1ac60dbc5be3ec888e08c5a49c8c1dda7f
sadatul <sadatulislamsadi@gmail.com>
2024-10-24 18:58:32 +0600

    Added a test

--- src/test/java/com/sadi/hackathon_authservice/HackathonAuthserviceApplicationTests.java
+++ src/test/java/com/sadi/hackathon_authservice/HackathonAuthserviceApplicationTests.java
@@ -1,13 +1,16 @@
 package com.sadi.hackathon_authservice;
 
+import com.sadi.hackathon_authservice.utils.CodeGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HackathonAuthserviceApplicationTests {
 
     @Test
     void contextLoads() {
+        String code = CodeGenerator.generateOtp();
+        Assertions.assertThat(code).isNotBlank();
     }
 
 }
